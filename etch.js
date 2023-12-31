7651c5ea67d23cf6897d37d16016610b1adaf555
Vuong Tran <121814098+Vtrandev@users.noreply.github.com>
2023-12-31 13:22:37 -0800

    Updated grid prompt and click to draw

--- etch.js
+++ etch.js
@@ -1,32 +1,39 @@
 const container = document.querySelector(".container");
 const btn = document.querySelector(".btn");
+let square;
 let numberOfSquares;
+let gridDimensionPercent;
 
 btn.addEventListener("click", (e) => {
   while (container.lastElementChild) {
     container.removeChild(container.lastElementChild);
   }
-
-  numberOfSquares = prompt("Number of squares");
+  numberOfSquares = 101;
+  while (numberOfSquares > 100) {
+    numberOfSquares = prompt("Number of squares across? (Less than 100)");
+  }
   buildGrid(numberOfSquares);
+  draw();
 });
 
 function buildGrid(numberOfSquares) {
-  let square;
+  gridDimensionPercent = (1 / numberOfSquares) * 100;
   for (let i = 0; i < numberOfSquares; i++) {
     for (let i = 0; i < numberOfSquares; i++) {
       square = document.createElement("div");
       square.classList.add("new");
-
+      square.style.width = `${gridDimensionPercent}%`;
+      square.style.height = `${gridDimensionPercent}%`;
       container.appendChild(square);
     }
   }
+}
 
+function draw() {
   const squareBox = document.querySelectorAll(".new");
-
   squareBox.forEach((square) => {
     square.addEventListener("mouseover", (e) => {
-      square.classList.add("selected");
+      if (e.buttons === 1) square.classList.add("selected");
     });
   });
 }
